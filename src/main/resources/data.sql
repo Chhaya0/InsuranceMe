05212922ad4f5ba93d90c8dd0771ff357c9ed479
Chhaya_P <chhayaprasad60@gmail.com>
2024-03-28 20:25:38 +0530

    changed about us

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+-- src/main/resources/data.sql
+
+INSERT INTO Policy (policy_holder_name, policy_type, policy_price, policy_start_date, policy_end_date) 
+VALUES ('John Doe', 'Health', 5000, '2024-01-01', '2025-01-01');
+
+INSERT INTO Policy (policy_holder_name, policy_type, policy_price, policy_start_date, policy_end_date) 
+VALUES ('Jane Smith', 'Car', 3000, '2024-02-01', '2025-02-01');
+
+-- Add more INSERT statements as needed
